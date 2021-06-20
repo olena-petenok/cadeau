157d34582c1392fa02e04de0ab0927729c55a4d4
Olena Petenok <olena.petenok.work@gmail.com>
2021-06-20 23:12:00 +0300

    main task completed

--- src/components/BlocksOfCardsWithShowMore/InfoCardWithShowMore/index.jsx
+++ src/components/BlocksOfCardsWithShowMore/InfoCardWithShowMore/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classnames from "classnames";
 
 import {
@@ -9,28 +9,44 @@ import {
 import "./index.scss";
 
 const InfoCardWithShowMore = ({ data = null }) => {
+  const { parentWidth, index, image, alt, name, description } = data;
+  const cardMaxWidth = 300;
+
   const [showMore, setShowMore] = useState(false);
+  const [cardOrder, setCardOrder] = useState({});
+  const [showMoreOrder, setShowMoreOrder] = useState({});
+
+  useEffect(() => {
+    if (parentWidth) {
+      const amountOfCardsPerRow = Math.round(parentWidth / cardMaxWidth);
+      const cardPosition = index % amountOfCardsPerRow;
+
+      const cardOrderFormula =
+        index <= amountOfCardsPerRow
+          ? index
+          : index * 2 - (cardPosition || amountOfCardsPerRow);
+
+      setCardOrder({ order: cardOrderFormula });
+      setShowMoreOrder({ order: cardOrderFormula + amountOfCardsPerRow });
+    }
+  }, [parentWidth, index]);
 
   const handleSetShowMore = () => setShowMore(true);
   const handleResetShowMore = () => setShowMore(false);
 
-  return data?.image && data?.alt && data?.name && data?.description ? (
+  return image && alt && name && description ? (
     <>
       <li
         className={classnames("info-card-with-show-more", {
           active: showMore,
         })}
-        key={`${data.index}card`}
+        style={cardOrder}
       >
-        <img
-          className="info-card-with-show-more-image"
-          alt={data.alt}
-          src={data.image}
-        />
+        <img className="info-card-with-show-more-image" alt={alt} src={image} />
 
         {!showMore && (
           <>
-            <p className="info-card-with-show-more-text-name">{data.name}</p>
+            <p className="info-card-with-show-more-text-name">{name}</p>
 
             <button
               className="info-card-with-show-more-button-show-more"
@@ -45,10 +61,10 @@ const InfoCardWithShowMore = ({ data = null }) => {
       {showMore && (
         <li
           className="info-card-with-show-more-description-block"
-          key={`${data.index}more`}
+          style={showMoreOrder}
         >
           <p className="info-card-with-show-more-description-block-text">
-            {data.description}
+            {description}
           </p>
 
           <button
